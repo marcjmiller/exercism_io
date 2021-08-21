697e1b389b6b216c38520a9d3eb8fd79473da310
Marc Miller <marcjmiller@outlook.com>
2021-08-20 23:03:41 -0400

    condensed matrix creation

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,16 +1,18 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix = matrix_string.splitlines()
-        for i, row in enumerate(self.matrix):
-            self.matrix[i] = row.split(" ")
-            for j, item in enumerate(self.matrix[i]):
-                self.matrix[i][j] = int(item)
+        # self.matrix = matrix_string.splitlines()
+        # for i, row in enumerate(self.matrix):
+        #     self.matrix[i] = row.split(" ")
+        #     for j, item in enumerate(self.matrix[i]):
+        #         self.matrix[i][j] = int(item)
+        #
+        # condensed
+        self.matrix = [
+            [int(item) for item in row.split()] for row in matrix_string.splitlines()
+        ]
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        result = []
-        for row in self.matrix:
-            result.append(row[index - 1])
-        return result
+        return [row[index - 1] for row in self.matrix]
